8e010129f131702c007a20e8f059627097437df2
handongyexingren <1340551309@qq.com>
2022-03-14 20:42:23 +0800

    张秀宏老师的书后面看不懂了，牙医的教程倒是能看懂，但我还是不会写啊，这。。。

--- examples/pineapple/main.go
+++ examples/pineapple/main.go
@@ -1,28 +1,29 @@
-package main 
+package main
 
 import (
-    "os"
-    "fmt"
-    "io/ioutil"
-    "github.com/karminski/pineapple/src"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	pineapple "pineapple/src"
 )
 
 func main() {
 
-    // read file
-    args := os.Args
-    if len(args) != 2 {
-        fmt.Printf("Usage: %s filename\n", args[0])
-        return
-    }
-    filename := args[1]
-    code, err := ioutil.ReadFile(filename)
-    if err != nil {
-        fmt.Printf("Error reading file: %s\n", filename)
-        return
-    }
+	// read file
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Printf("Usage: %s filename\n", args[0])
+		return
+	}
+	filename := args[1]
+	code, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", filename)
+		return
+	}
 
-    // execute
-    pineapple.Execute(string(code))
+	// execute
+	pineapple.Execute(string(code))
 
 }
